79090acab3bc178bd90d2f85c141072528aafe6f
pradow <pradochris+pradow@protonmail.com>
2022-10-05 21:04:29 -0700

    configure rollup js and sass to create either whole or minified file when requested

--- src/js/components/some-code.js
+++ src/js/components/some-code.js
@@ -1,4 +1,7 @@
-document.addEventListener('click', function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}, false);
+let greetButton = document.querySelector('#greetButton');
+
+function greetUser() {
+  console.log('Hello World!');
+}
+
+greetButton.addEventListener('click', greetUser);
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 import './components/_matches.polyfill.js';
 import './components/some-code.js';
-
-let greetButton = document.querySelector('#greetButton');
-
-function greetUser() {
-  console.log('Hello World');
-}
-
-greetButton.addEventListener('click', greetUser);
