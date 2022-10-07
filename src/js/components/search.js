f173a9e78e417c1b50715fc2d0fd2082db07e1fb
pradow <pradochris+pradow@protonmail.com>
2022-10-07 15:10:09 -0700

    add functions to format product list items

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -12,9 +12,34 @@ async function fetchData(url) {
 
 let storeData = async (url, cb) => {
   dataObject = await fetchData(url);
-  console.log(dataObject);
+  console.log(dataObject.products);
   cb();
 };
+
+function createResultItem(product) {
+  const productListItem = document.createElement('div');
+
+  productListItem.innerText = product.name;
+  return productListItem;
+}
+
+function populateResults() {
+  const results = [];
+  const fragment = document.createDocumentFragment();
+
+  dataObject.products.forEach((item) => {
+    const productInfo = {
+      name: item.product_name,
+    };
+    results.push(productInfo);
+  });
+
+  results.forEach((item) => {
+    fragment.append(createResultItem(item));
+  });
+
+  searchResults.append(fragment);
+}
 
 function populateData() {
   searchResults.innerText = JSON.stringify(dataObject);
@@ -25,7 +50,7 @@ function searchFood(e) {
 
   const searchLink = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchFoodInput.value}&page=1&search_simple=1&action=process&json=1`;
 
-  storeData(searchLink, populateData);
+  storeData(searchLink, populateResults);
 }
 
 searchFoodForm.addEventListener('submit', searchFood);
