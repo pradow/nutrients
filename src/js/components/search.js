ca72c506fa43f0de2dd591fa539f28a14c6021fc
pradow <pradochris+pradow@protonmail.com>
2022-10-08 14:21:05 -0700

    add productListItem attributes and listeners to populate selected item

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,3 +1,5 @@
+import { toggleModal, productInfoModal } from './modal.js';
+
 const searchFoodForm = document.querySelector('#searchFoodForm');
 const searchFoodInput = document.querySelector('#searchFoodInput');
 const searchResults = document.querySelector('#searchResults');
@@ -6,7 +8,6 @@ let dataObject;
 async function fetchData(url) {
   const results = await fetch(url);
   const data = await results.json();
-
   return data;
 }
 
@@ -16,10 +17,27 @@ let storeData = async (url, cb) => {
   cb();
 };
 
+function populateSelectedItemInfo(data) {
+  productInfoModal.innerText = `${data[0].id} ${data[0].product_name}`;
+  toggleModal();
+}
+
+function getSeletedProductInfo(e) {
+  const selectedItem = e.currentTarget;
+  const selectedItemInfo = dataObject.products.filter(
+    (item) => item.id === selectedItem.dataset.id
+  );
+
+  populateSelectedItemInfo(selectedItemInfo);
+}
+
 function createResultItem(product) {
   const productListItem = document.createElement('div');
 
   productListItem.innerText = product.name;
+  productListItem.setAttribute('data-id', product.id);
+  productListItem.classList.add('product-list-item');
+  productListItem.addEventListener('click', getSeletedProductInfo);
   return productListItem;
 }
 
@@ -30,6 +48,7 @@ function populateResults() {
   dataObject.products.forEach((item) => {
     const productInfo = {
       name: item.product_name,
+      id: item.id,
     };
     results.push(productInfo);
   });
